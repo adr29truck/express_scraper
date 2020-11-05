84a033c4e544c30f1ab606afb1459012a191f4fa
Jacob Sandström <jojacocobob@gmail.com>
2020-11-05 11:53:57 +0100

    removed p

--- modules/menu.rb
+++ modules/menu.rb
@@ -28,11 +28,9 @@ class Menu
   end
   
   def restaurant_menu
-    p "here"
     retries ||= 0
     data = []
     Scraper::Generic.new(@url).find_all('table')[1..3].each do |element|
-      p "here aswell"
       tr = element.find_all("tr")[0..1]
       data << { type: tr[0].find("div").text(:all)[0..-2],
                   dish: tr[1].find("div").text(:all) }
@@ -41,8 +39,6 @@ class Menu
   rescue StandardError
     retry if (retries += 1) < 3
     'Unable to fetch menu'
-    
-
   end
   # rubocop:enable Metrics/MethodLength
 end
